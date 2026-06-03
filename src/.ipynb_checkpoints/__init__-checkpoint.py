7b1589bd2c908f0a9e3a2ab0b7578a3b1cf6d294
kuanyuke <67796307+kuanyuke@users.noreply.github.com>
2026-06-04 00:11:08 +0200

    Delete src/.ipynb_checkpoints directory

--- src/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,6 +0,0 @@
-#!/bin/python
-from Baytomo.Targets import *
-from Baytomo.Models import Model
-from Baytomo.SingleChain import SingleChain
-from Baytomo.Plotting import PlotFromStorage
-from Baytomo.Sampling import Sampling
